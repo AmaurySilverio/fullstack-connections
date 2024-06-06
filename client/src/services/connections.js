abd3f5feaab79550a5c9b5676003e77597e66b20
AmaurySilverio <amaurycodes@gmail.com>
2024-06-05 21:41:14 -0400

    updated dist file

--- client/src/services/connections.js
+++ client/src/services/connections.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-const baseUrl =
-  "postgresql://admin:lW0JKBaIWUSygIj3nkSitqtMxufc1W9b@dpg-cpgc2s6ct0pc73db59n0-a.oregon-postgres.render.com/connections_bqpd";
 
 const getTitles = async () => {
   const response = await axios.get(`${import.meta.env.VITE_baseUrl}/cards`);
