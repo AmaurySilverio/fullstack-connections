53d75ec4eef8656adba0539c00273818640a4508
AmaurySilverio <amaurycodes@gmail.com>
2024-06-05 21:54:38 -0400

    added packages

--- client/src/services/connections.js
+++ client/src/services/connections.js
@@ -1,18 +1,15 @@
 import axios from "axios";
 
 const getTitles = async () => {
-  const response = await axios.get(`${import.meta.env.VITE_baseUrl}/cards`);
+  const response = await axios.get(`${baseUrl}/cards`);
   return response.data;
 };
 const getCustomGame = async (id) => {
-  const response = await axios.get(`${import.meta.env.VITE_baseUrl}`);
+  const response = await axios.get(`${baseUrl}`);
   return response.data;
 };
 const create = async (newCustomGame) => {
-  const response = await axios.post(
-    `${import.meta.env.VITE_baseUrl}/cards`,
-    newCustomGame
-  );
+  const response = await axios.post(`${baseUrl}/cards`, newCustomGame);
   console.log(response);
   return response.data;
 };
