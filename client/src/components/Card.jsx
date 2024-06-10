7a56580a397bfeb69c09cc93ac827f2c49984932
AmaurySilverio <amaurycodes@gmail.com>
2024-06-10 08:28:56 -0400

    dynamic font sizing

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
 const Card = ({ name, onClick, category, difficulty, id }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    handleMediaQueryChange(mediaQuery); // Set initial state
+    mediaQuery.addEventListener("change", handleMediaQueryChange); // Add listener for future changes
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange); // Cleanup listener on unmount
+    };
+  }, []);
   return (
     <>
       <div
@@ -7,6 +23,7 @@ const Card = ({ name, onClick, category, difficulty, id }) => {
         onClick={onClick}
         data-category={category}
         data-difficulty={difficulty}
+        style={isMobile && name.length >= 9 ? { fontSize: "0.85em" } : {}}
       >
         {name}
       </div>
